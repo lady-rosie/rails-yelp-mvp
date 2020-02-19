8dce1dfa0a1970208924e8e3dccb1a5d5b3f69fb
Rose Grondin <rose.grondin@gmail.com>
2020-02-19 13:00:09 -0500

    create a yelp mvp with the restaurant controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create({name: 'Makoto', address: '7112 St Laurent Blvd,', phone_number: '(514) 272-7112', category:'japanese'})
+Restaurant.create({name: 'Restaurant Corneli', address: '6741 St Laurent Blvd,', phone_number: '(514) 325-1300', category:'italian'})
+Restaurant.create({name: 'Shô-Dan', address: '2020 Metcalfe St', phone_number: '(514) 987-9987', category:'japanese'})
+Restaurant.create({name: "L'Express", address: '3927 St Denis St', phone_number: '(514) 845-5333', category:'french'})
+Restaurant.create({name: 'Wing Fa', address: '3474 Park Ave', phone_number: '(514) 282-3938', category:'chinese'})
