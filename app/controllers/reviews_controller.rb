963c33cb3a0fcbacc8bfed6bb551efdb707e50e0
Rose Grondin <rose.grondin@gmail.com>
2020-02-19 14:04:01 -0500

    finish the creation of the review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,23 @@
 class ReviewsController < ApplicationController
-  def index
-  end
   def create
+    restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(safe_params)
+    @review.restaurant = restaurant
+    if @review.save
+      redirect_to restaurant_path(restaurant)
+    else
+      render 'new'
+    end
   end
-  def show
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  private
+
+  def safe_params
+    params.require(:review).permit(:content, :rating)
   end
 end
